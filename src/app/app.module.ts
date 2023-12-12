5dd00fe7d03a5496cb989b1dd95f575d63b93928
sistemasinea <sistemasinea@gmail.com>
2023-12-12 14:40:56 -0300

    feat: cria modulo home e importa modulos primneng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 
+//PrimeNG
+import { CardModule } from 'primeng/card'
+import { InputTextModule } from 'primeng/inputtext'
+import { ButtonModule } from 'primeng/button'
+import { ToastModule } from 'primeng/toast'
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +26,11 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    //PrimeNG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
