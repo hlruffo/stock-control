6c0df60a8e024c0cf82406a9e9b7f31028487543
sistemasinea <sistemasinea@gmail.com>
2023-12-12 16:02:58 -0300

    feat: cria as funções reativas do form de cadastro e login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,29 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   loginCard: boolean = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  signUpForm = this.formBuilder.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    senha: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) {
+
+
+  };
+
+  onSubmitLoginForm(): void {
+    console.log(this.loginForm.value)
+  };
+
+  onSubmitsignUpForm(): void {
+    console.log(this.signUpForm.value)
+  };
 }
+
+
